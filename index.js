5d9212922507e268cddfe534c383a1393a4c6723
Moyboy <Vincent.moy94@gmail.com>
2024-08-28 23:23:00 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+function submitData (userName, userEmail) {
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    })
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (user) {
+        const newID = user.id
+        const idElement = document.createElement('p');
+        idElement.textContent = newID;
+        document.body.appendChild(idElement);
+        })
+        .catch(function (error) {
+        const errorMessage = error.message;
+        const errorElement = document.createElement('p');
+        errorElement.textContent = errorMessage;
+        document.body.appendChild(errorElement);
+        })
+}
